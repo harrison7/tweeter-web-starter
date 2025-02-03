985f2ce9cc18633dced70153952f315c6758eae3
harrison7 <harrison@thecaspers.net>
2025-02-03 13:22:09 -0700

    Test implementation of userNavigationHook

---
+++ tweeter-web-starter/tweeter-web/src/components/statusItem/userNavigationHook.ts
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import useToastListener from "../toaster/ToastListenerHook";
+import { UserInfoContext } from "../userInfo/UserInfoProvider";
+import { AuthToken, FakeData, User } from "tweeter-shared";
+
+const useUserNavigation = async (event: React.MouseEvent): Promise<void> => {
+    const { displayErrorMessage } = useToastListener();
+    const { setDisplayedUser, currentUser, authToken } =
+        useContext(UserInfoContext);
+
+    event.preventDefault(); 
+
+    try {
+        const alias = extractAlias(event.target.toString());
+
+        const user = await getUser(authToken!, alias);
+
+        if (!!user) {
+            if (currentUser!.equals(user)) {
+                setDisplayedUser(currentUser!); 
+            } else {
+                setDisplayedUser(user);
+            }
+        }
+    } catch (error) {
+        displayErrorMessage(`Failed to get user because of exception: ${error}`);
+    }
+};
+
+const extractAlias = (value: string): string => {
+    const index = value.indexOf("@");
+    return value.substring(index);
+};
+
+const getUser = async (
+    authToken: AuthToken,
+    alias: string
+): Promise<User | null> => {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.findUserByAlias(alias);
+};
+
+export default useUserNavigation;
